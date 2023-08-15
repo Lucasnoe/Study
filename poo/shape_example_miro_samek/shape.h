e72ca0101064d8a80b752ea9d6f1b0afd967af4a
Lucasnoe <lucas.nooe@outlook.com>
2023-08-15 14:02:31 -0300

    poo study - shape example Miro Samek

---
+++ poo/shape_example_miro_samek/shape.h
@@ -0,0 +1,22 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <stdint.h>
+
+// Implementação da classe "Shape"
+
+typedef struct {
+    int16_t x;
+    int16_t y;
+} Shape;            
+
+// Constructor serve para inicializar um objeto da classe "Shape"
+
+void Shape_ctor(Shape * const me, int16_t x0, int16_t y0);  // "me" é um ponteiro constante que aponta para um dado do tipo Shape
+
+void Shape_moveBy(Shape * const me, int16_t dx, int16_t dy);
+
+uint16_t Shape_distanceFrom(const Shape * const me, Shape const * other);
+
+
+#endif
